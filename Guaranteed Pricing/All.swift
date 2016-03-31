c1eadae9ba4332b1d34657fcafb03de93236e05e
adrianables <depauw@DePauws-MacBook-Pro-5.local>
2016-03-30 21:42:50 -0400

    Push new updates with login

--- Guaranteed Pricing/All.swift
+++ Guaranteed Pricing/All.swift
@@ -1,9 +1,57 @@
+
 //
 //  All.swift
 //  Guaranteed Pricing
 //
-//  Created by DePauw on 3/21/16.
+//  Created by DePauw on 3/30/16.
 //  Copyright © 2016 DePauw. All rights reserved.
 //
 
 import Foundation
+import UIKit
+import Firebase
+
+class All: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+     var items: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        let ref = Firebase(url:"https://sizzling-inferno-451.firebaseio.com/services")
+        ref.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            // do some stuff once
+            print(snapshot.value)
+            
+            // get these values and put them in the cell's text view. key is more important
+            print(snapshot.key)
+            print(snapshot.value["description"])
+            
+            // add to the array and just this array
+            self.items.append(snapshot.key)
+        })
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+
+    }
+    
+    
